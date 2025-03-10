f1ceb1ad56c842589e036ff7f00725bb2c126b08
Filipe-Mesquita <filipeslb2004@gmail.com>
2025-03-10 15:47:46 +0000

    Tree object start

---
+++ Treetening/Assets/Scripts/treeScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class treeScript : MonoBehaviour
+{
+    [SerializeField] private float hp;
+    [SerializeField] private float rootValue;
+    [SerializeField] private string treeName;
+    [SerializeField] private Mesh treeMesh;
+    [SerializeField] private Mesh rootMesh;
+
+    void Start()
+    {
+
+    }
+}
